6825f5a4c4acd87c42126f8351bce77b2e2d4d13
v-eyvazov <60714328+v-eyvazov@users.noreply.github.com>
2022-02-22 18:10:27 +0400

    Fixed Authentication urls

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, include
-from user_account.views import registration_view, fetch_token, login_view, logout_view
+from django.urls import path
+from user_account.views import registration_view, login_view, logout_view
 
 urlpatterns = [
     path('register', registration_view, name='register'),
-    path('', include('django.contrib.auth.urls')),
-    path('fetch-token', fetch_token, name='fetch-token'),
     path('login', login_view, name='login'),
     path('logout', logout_view, name='logout')
 ]
